064987f1c298f45b92fab0984334f6ada6f998d0
necula_lidia <necula_lidia@yahoo.com>
2020-05-02 14:17:47 +0200

    Replace loops with Linq queries

---
+++ IteratorDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorDemo
+{
+    class Program
+    {
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return painters
+                .Where(x => x.IsAvailable)
+                .WithMinimum(x => x.EstimateCompensation(sqMeters));
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
