13dc705c931ebb5a822183eccd29fa41435f07d7
necula_lidia <necula_lidia@yahoo.com>
2020-05-03 20:33:40 +0200

    Add immutable class MoneyAmount

---
+++ ValueObjectDemo/MoneyAmount.cs
@@ -0,0 +1,23 @@
+﻿namespace ValueObjectDemo
+{
+    public class MoneyAmount
+    {
+        public decimal Amount { get; }
+        public string CurrencySymbol { get; }
+
+        public MoneyAmount(decimal amount, string currencySymbol)
+        {
+            Amount = amount;
+            CurrencySymbol = currencySymbol;
+        }
+
+        public MoneyAmount Scale(decimal factor)
+           => new MoneyAmount(Amount * factor, CurrencySymbol);
+
+        public static MoneyAmount operator *(MoneyAmount amount, decimal factor)
+            => amount.Scale(factor);
+
+        public override string ToString()
+            => $"{Amount} {CurrencySymbol}";
+    }
+}
