cc7d9db9c967450ebf77832dc216f5d9eff81eca
necula_lidia <necula_lidia@yahoo.com>
2020-05-03 21:33:47 +0200

    Add the equality operator

---
+++ ValueObjectEqualityOperatorDemo/MoneyAmount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ValueObjectEqualityOperatorDemo
+{
+    public sealed class MoneyAmount : IEquatable<MoneyAmount>
+    {
+        public decimal Amount { get; }
+        public string CurrencySymbol { get; }
+
+        public MoneyAmount(decimal amount, string currencySymbol)
+        {
+            Amount = amount;
+            CurrencySymbol = currencySymbol;
+        }
+
+        public MoneyAmount Scale(decimal factor)
+           => new MoneyAmount(Amount * factor, CurrencySymbol);
+
+        public static MoneyAmount operator *(MoneyAmount amount, decimal factor)
+            => amount.Scale(factor);
+
+        public override bool Equals(object obj)
+            => Equals(obj as MoneyAmount);
+
+        public bool Equals(MoneyAmount other)
+            => other != null &&
+               Amount == other.Amount &&
+               CurrencySymbol == other.CurrencySymbol;
+
+        public override int GetHashCode()
+            => Amount.GetHashCode() ^ CurrencySymbol.GetHashCode();
+
+        public static bool operator ==(MoneyAmount a, MoneyAmount b)
+            => (object.ReferenceEquals(a, null) && object.ReferenceEquals(b, null)) ||
+            (!object.ReferenceEquals(a, null) && a.Equals(b));
+
+        public static bool operator !=(MoneyAmount a, MoneyAmount b)
+            => !(a == b);
+
+        public override string ToString()
+            => $"{Amount} {CurrencySymbol}";
+    }
+}
---
+++ ValueObjectEqualityOperatorDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValueObjectEqualityOperatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoneyAmount x = new MoneyAmount(2, "USD");
+            MoneyAmount y = new MoneyAmount(4, "USD");
+
+            if (x.Equals(y))
+                Console.WriteLine("Equal.");
+            if (y.Equals(x * 2))
+                Console.WriteLine("Equal after scaling.");
+
+            MoneyAmount a = new MoneyAmount(2, "USD");
+            MoneyAmount b = new MoneyAmount(2, "USD");
+
+            HashSet<MoneyAmount> set = new HashSet<MoneyAmount>();
+
+            set.Add(a);
+
+            if (set.Contains(b))
+                Console.WriteLine("Cannot add repeated value.");
+            else
+                set.Add(b);
+
+            Console.WriteLine("Count = {0}", set.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
