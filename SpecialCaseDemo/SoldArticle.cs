71d58076b20fea45bfad2dc70539ee515597c956
necula_lidia <necula_lidia@yahoo.com>
2020-05-05 16:29:51 +0200

    Apply the state pattern

--- SpecialCaseDemo/SoldArticle.cs
+++ SpecialCaseDemo/SoldArticle.cs
@@ -4,13 +4,26 @@ namespace SpecialCaseDemo
 {
     public class SoldArticle
     {
-        public IWarranty MoneyBackGurantee { get; }
-        public IWarranty ExpressWarranty { get; }
+        public IWarranty MoneyBackGurantee { get; private set; }
+        public IWarranty ExpressWarranty { get; private set; }
+        private IWarranty NotOperationalWarranty { get; }
 
         public SoldArticle(IWarranty moneyBackGurantee, IWarranty expressWarranty)
         {
             MoneyBackGurantee = moneyBackGurantee ?? throw new ArgumentNullException(nameof(moneyBackGurantee));
             ExpressWarranty = expressWarranty ?? throw new ArgumentNullException(nameof(expressWarranty));
+            NotOperationalWarranty = expressWarranty;
+        }
+
+        public void VisibleDamage()
+        {
+            MoneyBackGurantee = VoidWarranty.Instance;
+        }
+
+        public void NotOperational()
+        {
+            MoneyBackGurantee = VoidWarranty.Instance;
+            ExpressWarranty = NotOperationalWarranty;
         }
     }
 }
